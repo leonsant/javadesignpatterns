04942b5c7ad57489ac339a277a3105e26e118682
Leonardo Santos <leonardo.1.santos@nokia.com>
2022-12-03 19:11:46 +0000

    Passing ChatRoom into ChatUser

--- observer/src/main/java/pt/leo/observer/core/ChatUser.java
+++ observer/src/main/java/pt/leo/observer/core/ChatUser.java
@@ -2,15 +2,15 @@ package pt.leo.observer.core;
 
 public class ChatUser implements Observer {
     String name;
-    Observable observable;
+    ChatRoom chatRoom;
 
-    public ChatUser(String name, Observable observable) {
+    public ChatUser(String name, ChatRoom chatRoom) {
         this.name = name;
-        this.observable = observable;
+        this.chatRoom = chatRoom;
     }
 
     @Override
     public void update() {
-        System.out.println("  " + name + "'s holidays > " + observable.getData());
+        System.out.println("  " + name + "'s holidays > " + chatRoom.getData());
     }
 }
